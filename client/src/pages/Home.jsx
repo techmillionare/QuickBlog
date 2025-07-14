21e84538cbce6192bb5fe2b637e95fb148a5e3c5
techmillionare <dayanandmishra987@gmail.com>
2025-07-15 00:26:09 +0530

    Fix Header import path for case‑sensitive build

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import BlogList from '../components/BlogList'
-import Header from '../components/header'
+import Header from '../components/Header'
 import NewsLetter from '../components/NewsLetter'
 import Footer from '../components/Footer'
 
