0ea2ca4a4559873388ce5fee6d4cb3faa790c39f
techmillionare <dayanandmishra987@gmail.com>
2025-08-07 01:29:54 +0530

    Gemini AI feature added

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -76,7 +76,7 @@ const Blog = () => {
             mx-auto text-gray-800'>{data.title}</h1>
             <h2 className='my-5 max-w-lg truncate mx-auto'>{data.subTitle}</h2>
             <p className='inline-block py-1 px-4 rounded-full mb-6 border text-sm
-          border-primary/35 bg-primary/5 font-medium text-primary'>Mohit agarwaal</p>
+          border-primary/35 bg-primary/5 font-medium text-primary'>Dayanand Mishra</p>
         </div>
 
         <div className='mx-5 max-w-5xl md:mx-auto my-10 mt-6'>
--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -29,7 +29,7 @@ const AddBlog = () => {
       setLoading(true)
       const {data} = await axios.post('/api/blog/generate', {prompt: title})
       if(data.success){
-        quillRef.current.roor.innerHTML = parse(data.content)
+        quillRef.current.root.innerHTML = parse(data.content)
       }
       else{
         toast.error(data.message)
@@ -105,6 +105,12 @@ const AddBlog = () => {
         <p className='mt-4'>Blog Description</p>
         <div className='max-w-lg h-74 pb-16 sm:pb-10 pt-2 relative'>
           <div ref={editorRef}></div>
+          {loading && (
+            <div className='absolute right-0 top-0 left-0 bottom-0 flex items-center
+            justify-center bg-black/10 mt-2'>
+              <div className='w-8 h-8 rounded-full border-t-white
+              animate-spin'></div>
+            </div>)}
             <button disabled={loading} type='button' onClick={generatecontent} className='absolute bottom-1 right-2 ml-2 text-xs
             text-white bg-black/70 px-4 py-1.5 rounded
             hover:underline cursor-pointer'
